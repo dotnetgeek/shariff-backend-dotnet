3b10b149976288314bddfdf8ae788e22a2c98f31
Daniel Müller <dm@telligene-consulting.com>
2016-10-23 12:35:30 +0200

    Use DeveloperExceptionPage only in DEV Enviroment

--- src/Startup.cs
+++ src/Startup.cs
@@ -8,7 +8,8 @@ namespace Shariff.Backend
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(
+            IHostingEnvironment env)
         {
             var builder = new ConfigurationBuilder()
                 .SetBasePath(env.ContentRootPath)
@@ -21,19 +22,25 @@ namespace Shariff.Backend
         public IConfigurationRoot Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(
+            IServiceCollection services)
         {
             // Add framework services.
             services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(
+            IApplicationBuilder app, 
+            IHostingEnvironment env, 
+            ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
             loggerFactory.AddDebug();
 
-            app.UseDeveloperExceptionPage();
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+
             app.UseMvc();
             
         }
